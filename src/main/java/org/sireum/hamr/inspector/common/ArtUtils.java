d98f1ede37de0c306e70fe2be5b9b33ddd0061b8
Matthew Weis <matthewweiscaps@gmail.com>
2020-04-16 17:45:46 -0500

    0.4-SNAPSHOT release

--- src/main/java/org/sireum/hamr/inspector/common/ArtUtils.java
+++ src/main/java/org/sireum/hamr/inspector/common/ArtUtils.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2020, Matthew Weis, Kansas State University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.sireum.hamr.inspector.common;
 
 import art.Bridge;
@@ -155,11 +180,11 @@ public class ArtUtils {
     }
 
     public String informativePrettyPrint(@NotNull UPort port) {
-        return String.format("%s [%s]", prettyPrint(port, getBridge(port)), port.mode().toString());
+        return String.format("%s (%s) [%s]", prettyPrint(port, getBridge(port)), port.mode().toString(), port.id().toString());
     }
 
     public String informativePrettyPrint(@NotNull Bridge bridge) {
-        return String.format("%s [%s]", prettyPrint(bridge), bridge.dispatchProtocol().toString());
+        return String.format("%s (%s) [%s]", prettyPrint(bridge), bridge.dispatchProtocol().toString(), bridge.id().toString());
     }
 
     private String findCommonBridgePrefix() {
